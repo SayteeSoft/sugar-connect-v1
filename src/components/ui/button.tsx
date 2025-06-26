3b6e6e784ddcbd35ddce7a7f9674ae5d54d0c2d6
Larry Saytee <saytee.software@web-developer.one>
2025-06-26 14:23:04 +0000

    make mouse over hover affect the same that being white background with p

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-primary hover:text-primary-foreground",
+          "bg-secondary text-secondary-foreground hover:bg-card hover:text-primary",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
