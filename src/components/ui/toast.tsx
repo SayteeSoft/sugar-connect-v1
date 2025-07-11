dd74ad7fa76512127474e18b1c8b2332b4a016ea
Larry Saytee <saytee.software@web-developer.one>
2025-07-11 16:07:11 +0000

    ok please have all toast pop up notifications just below the header

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -23,9 +23,9 @@ const ToastViewport = React.forwardRef<
         <ToastPrimitives.Viewport
             ref={ref}
             className={cn(
-                "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 md:max-w-[420px]",
+                "fixed top-16 z-[100] flex max-h-screen w-full flex-col-reverse p-4 md:max-w-[420px]",
                 // Apply responsive classes only on the client to avoid hydration mismatch
-                mounted ? "sm:top-0 sm:right-0 sm:flex-col" : "sm:bottom-0 sm:flex-col-reverse",
+                mounted ? "sm:right-0 sm:flex-col" : "sm:bottom-0 sm:flex-col-reverse",
                 className
             )}
             {...props}
