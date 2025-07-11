d6b3540e2163f618d0821e68f18d90f0065f45b4
Larry Saytee <saytee.software@web-developer.one>
2025-07-11 18:48:31 +0000

    change the title in the Header and Footer from "SD Connect" to "Sugar Co

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,7 +4,7 @@ export const Logo = ({ className }: { className?: string }) => (
   <div className={`flex items-center gap-2 font-bold font-headline ${className}`}>
     <Heart className="h-6 w-6 text-primary" />
     <span className="text-primary">
-        SD Connect
+        Sugar Connect
     </span>
   </div>
 );
