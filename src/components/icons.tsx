447ec7a08dfe0958942dbdc3e4e72409c7639c9c
Larry Saytee <saytee.software@web-developer.one>
2025-07-11 21:33:11 +0000

    create the size of the title "Sugar Connect" in the Header.

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,7 +3,7 @@ import { Heart } from 'lucide-react';
 export const Logo = ({ className }: { className?: string }) => (
   <div className={`flex items-center gap-2 font-bold font-headline ${className}`}>
     <Heart className="h-6 w-6 text-icon-primary" />
-    <span className="text-primary">
+    <span className="text-primary text-xl">
         Sugar Connect
     </span>
   </div>
