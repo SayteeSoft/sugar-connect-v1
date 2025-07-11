116ca31ffe81aff663bacf838a306ddc0f29562d
Larry Saytee <saytee.software@web-developer.one>
2025-07-11 16:48:15 +0000

    PLEASE FIX "Runtime Error Server
    ReferenceError: React is not defined

--- src/app/purchase-credits/page.tsx
+++ src/app/purchase-credits/page.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
