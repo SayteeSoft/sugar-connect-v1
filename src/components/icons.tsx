1a7ce83ac533ab37a4af1f248e5ba718333dfeca
Larry Saytee <saytee.software@web-developer.one>
2025-07-11 22:38:01 +0000

    in the Header make the "Sugar Connect" font 1x thicker

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,7 +1,7 @@
 import { Heart } from 'lucide-react';
 
 export const Logo = ({ className }: { className?: string }) => (
-  <div className={`flex items-center gap-2 font-bold font-headline ${className}`}>
+  <div className={`flex items-center gap-2 font-extrabold font-headline ${className}`}>
     <Heart className="h-6 w-6 text-icon-primary" />
     <span className="text-primary text-xl">
         Sugar Connect
