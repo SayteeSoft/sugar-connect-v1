20d4bc612ca4814b8d26b32e4aa6b0cdfaebc806
Larry Saytee <saytee.software@web-developer.one>
2025-07-12 10:53:52 +0000

    make the heart icon in the Header and Footer color "#f5a3f5" in dark mod

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,7 +3,7 @@ import { Heart } from 'lucide-react';
 
 export const Logo = ({ className }: { className?: string }) => (
   <div className={`flex items-center gap-2 font-extrabold font-headline ${className}`}>
-    <Heart className="h-5 w-5 text-icon-primary fill-current" />
+    <Heart className="h-5 w-5 text-heart-icon fill-current" />
     <span className="text-primary text-xl">
         Sugar Connect
     </span>
